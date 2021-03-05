ca25d849e1743c26beae877dd7301d9c25b23317
Akash papnai <akashpapnai@gmail.com>
2021-03-05 16:53:31 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,8 +3,10 @@ from bot import telegram_chatbot
 bot = telegram_chatbot("config.cfg")
 
 
-def make_reply(msg):
-    return msg
+def make_reply(msg, name):
+    if msg is not None and "hello".upper() in msg.upper():
+        return "Hi"
+    return "Hello "+name
     
 
 update_id = None
@@ -15,10 +17,12 @@ if updates:
     update_id = updates[len(updates)-1]["update_id"]
     try:
         message = str(updates[len(updates)-1]["message"]["text"])
+        name = str(updates[len(updates)-1]['message']["chat"]["first_name"])
         print(message)
     except:
         message = None
+        name = None
     from_ = updates[len(updates)-1]["message"]["from"]["id"]
-    reply = make_reply(message)
+    reply = make_reply(message, name)
     print(from_,reply)
     bot.send_message(reply, from_)
