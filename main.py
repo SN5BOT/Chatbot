fa9ff00b359cd4970b2252582e8e1b1bcc10d165
SN5BOT <102501213+SN5BOT@users.noreply.github.com>
2022-03-30 08:58:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import configparser as cfg
 print("Bot started")
 
 def start_command(update,context):
-    update.message.reply_text('Type something to get started!{chat.id}')
+    update.message.reply_text('Type something to get 😂 started!{chat.id}')
 
 def help_command(update, context):
     update.message.reply_text('More help? Find on Google')
 
 def handle_message(update, context):
     text = str(update.message.text).lower()
-    text = str(update.message.chat.id).lower()
     response = res.sample_responses(text)
     update.message.reply_text(response)
 
